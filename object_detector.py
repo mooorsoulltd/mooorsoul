ec3a257c335cd921243ec578120eea2c270ce78f
mooorsoulltd <mooorsoulltd@gmail.com>
2026-03-04 01:11:32 +0000

    Add object_detector.py for detecting objects in video frames using pre-trained models

---
+++ object_detector.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+class ObjectDetector:
+    def __init__(self, model_path, config_path):
+        self.net = cv2.dnn.readNetFromDarknet(config_path, model_path)
+        self.layer_names = self.net.getLayerNames()
+        self.output_layers = [self.layer_names[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
+
+    def detect_objects(self, frame):
+        height, width, _ = frame.shape
+        blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+        self.net.setInput(blob)
+        outputs = self.net.forward(self.output_layers)
+
+        boxes = []
+        confidences = []
+        class_ids = []
+
+        for output in outputs:
+            for detection in output:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        return [(boxes[i], class_ids[i]) for i in indexes.flatten()]
